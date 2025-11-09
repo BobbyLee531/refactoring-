ec5057e6d75002fe2f3cba87bfa7adcfbc22fb47
bobbyliyz <82566476+BobbyLee531@users.noreply.github.com>
2025-11-09 18:49:19 -0500

    code style finished

--- src/main/java/theater/Performance.java
+++ src/main/java/theater/Performance.java
@@ -1,15 +1,35 @@
 package theater;
 
 /**
- * Class representing a performance of a play..
+ * Class representing a performance of a play.
  */
 public class Performance {
 
-    public String playID;
-    public int audience;
+    private String playID;
+    private int audience;
 
     public Performance(String playID, int audience) {
         this.playID = playID;
+        this.audience = audience;
+    }
+
+    // Getter for playID
+    public String getPlayID() {
+        return playID;
+    }
+
+    // Setter for playID
+    public void setPlayID(String playID) {
+        this.playID = playID;
+    }
+
+    // Getter for audience
+    public int getAudience() {
+        return audience;
+    }
+
+    // Setter for audience
+    public void setAudience(int audience) {
         this.audience = audience;
     }
 }
--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,41 @@
 package theater;
 
+/**
+ * Represents a play with a name and type.
+ * @null no fields in this class should be null
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    /**
+     * Name of the play.
+     * @null never
+     */
+    private String name;
+
+    /**
+     * Type of the play, e.g. "tragedy", "comedy".
+     * @null never
+     */
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
         this.type = type;
     }
 }
